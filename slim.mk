caefd900c6582b50debca38b69f077b64d2b2b85
blk_jack <jcorcoran+github@gmail.com>
2013-05-08 02:06:59 -0400

    Initial commit for jfltecan

---
+++ slim.mk
@@ -0,0 +1,32 @@
+$(call inherit-product, device/samsung/jfltecan/full_jfltecan.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := jfltecan
+
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/slim/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+
+# Inherit device settings
+$(call inherit-product, vendor/slim/config/common_sgs.mk)
+
+# Inherit torch settings
+$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+
+PRODUCT_COPY_FILES +=  \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevl TARGET_DEVICE=jfltecan BUILD_FINGERPRINT="samsung/jfltevl/jfltecan:4.2.2/JDQ39/I337MVLUAMDJ:user/release-keys" PRIVATE_BUILD_DESC="jfltevl-user 4.2.2 JDQ39 I337MVLUAMDJ release-keys"
+
+PRODUCT_NAME := slim_jfltecan
+PRODUCT_DEVICE := jfltecan
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SGH-I337M
+PRODUCT_MANUFACTURER := Samsung
+

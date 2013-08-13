8c8045104159fa6c050a9c747ae4e745f2e0134b
blk_jack <jcorcoran+github@gmail.com>
2013-08-14 01:16:19 +0200

    4.3 bringup
    Change-Id: Idbb06172fe338817e60c307632ce9007efa965aa

--- slim.mk
+++ slim.mk
@@ -19,9 +19,6 @@ $(call inherit-product, vendor/slim/config/common_sgs.mk)
 # Inherit torch settings
 $(call inherit-product, vendor/slim/config/common_ledflash.mk)
 
-PRODUCT_COPY_FILES +=  \
-    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevl TARGET_DEVICE=jfltecan BUILD_FINGERPRINT="samsung/jfltevl/jfltecan:4.2.2/JDQ39/I337MVLUAMDJ:user/release-keys" PRIVATE_BUILD_DESC="jfltevl-user 4.2.2 JDQ39 I337MVLUAMDJ release-keys"
 
 PRODUCT_NAME := slim_jfltecan

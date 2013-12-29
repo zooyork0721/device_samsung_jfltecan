a1a0256bf0e4e638337f2671cf1e9563bbd6ff4c
Lars Greiss <kufikugel@googlemail.com>
2013-12-29 21:22:20 +0100

    jfltecan: remove common_led overlay
    Change-Id: Iab8158fe2f7e55591c14d944d35adb1176518d58

--- slim.mk
+++ slim.mk
@@ -12,9 +12,6 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 # Inherit some common SLIM stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-# Inherit torch settings
-$(call inherit-product, vendor/slim/config/common_ledflash.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevl TARGET_DEVICE=jfltecan BUILD_FINGERPRINT="samsung/jfltevl/jfltecan:4.2.2/JDQ39/I337MVLUAMDJ:user/release-keys" PRIVATE_BUILD_DESC="jfltevl-user 4.2.2 JDQ39 I337MVLUAMDJ release-keys"
 
 PRODUCT_NAME := slim_jfltecan
